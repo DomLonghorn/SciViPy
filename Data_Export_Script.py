7a17081ce86dd12871b3b77683df01bfa2b3be3e
Freddie <FWKCarlisle@gmail.com>
2022-07-28 17:24:25 +0100

    Adding the Data Export Script

---
+++ Data_Export_Script.py
@@ -0,0 +1,24 @@
+from paraview.simple import *
+from paraview.servermanager import * #MAKE SURE TO INCLUDE THIS MODULE WHEN LOADING VTK FILES!!!!!!!!!!!!
+import vtk
+
+
+InitialVal = 5030 #Initial numerical value for data entry
+
+
+for i in range(1):
+    additive = i * 10 #All files are an addition of 10 on the previous file
+    CurrentVal = InitialVal + additive
+    StringVal = str(CurrentVal) #Converted to a string to allow file reading
+    
+    Currentfile = "/home/user/Desktop/JOREK_data/MAST-U_processed_v2/jorek0"+StringVal+".pvsm"    
+    #reader = OpenDataFile(Currentfile) #This reads the file into the code
+    #reader.GetPointDataInformation() #This processes the data arrays within the vtk file, allowing them to be processed
+    reader = LoadState(Currentfile)
+    #if reader:
+        #print("success, current iteration is "+str(i)) #Ensures file has been read successfully 
+    #else:
+        #print("failed")
+
+    SaveData("/home/user/Desktop/JOREK_data/MAST-U_processed_v2/TESTjorek0"+StringVal+".vtk",proxy=None)
+    ResetSession()

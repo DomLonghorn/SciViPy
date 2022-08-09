6facebec8ed76191d3ed05c61edb815ad669428c
FWKCarlisle <110162827+FWKCarlisle@users.noreply.github.com>
2022-08-09 16:38:53 +0100

    m today's adjustments and proper coding progress

--- xyzConverterScript.py
+++ xyzConverterScript.py
@@ -1,27 +1,41 @@
 #Data = open("/home/user/Desktop/Data/Max data/w_220_cascade.00500.voxels.xyz","r")
+from re import X
+
+
 noofdatapoints = 10
 count = 0
-convertedFile = open("C:\\Users\\FWKCa\\OneDrive\\Desktop\\Max Data\\Testxyz.txt","w")
+DataFile = open("C:\\Users\\FWKCa\\OneDrive\\Desktop\\Max Data\\DataTXT.txt","w")
+InfoFile = open("C:\\Users\\FWKCa\\OneDrive\\Desktop\\Max Data\\InfoTXT.txt","w")
+
+
 Dimensions = []
 
 # with open("C:\\Users\\FWKCa\\OneDrive\\Desktop\\Max Data\\w_220_cascade.00500.voxels.xyz","r") as Data:
-#     datapoints = Data.readlines()[1:(noofdatapoints+2)]
-#     print(Data.readline())
-#     Dimensions.append(Data.readlines()[2])
+#     ExtraInfo = Data.readlines()[0:2]
+#     datapoints = Data.readlines()[2:(noofdatapoints+2)]
+#     #Dimensions.append(Data.readlines())
+# #print(datapoints)
+# print(ExtraInfo)
+# print(datapoints)
+
+infoLines = []
+dataLines = []
 Data = open("C:\\Users\\FWKCa\\OneDrive\\Desktop\\Max Data\\w_220_cascade.00500.voxels.xyz","r")
 
-for x in Data range(1:(noofpoints+2)):
-    datapoints = x 
+for lines in range(noofdatapoints):
+    lines = Data.readline()
+    if count <= 1:
+        infoLines.append(lines)
+        count+=1 
+    elif count <= noofdatapoints:
+        dataLines.append(lines)
+        count+=1
+    else: pass
+#print(infoLines)
+print(dataLines)
 
-
-
-
-#print(Dimensions)
-
-
-
-
-for i in range(0, noofdatapoints):
-    convertedFile.write(datapoints[i])
-    
+for i in range(0, noofdatapoints-2):
+    DataFile.write(dataLines[i])
+for x in range(2):
+    InfoFile.write(infoLines[x])
 

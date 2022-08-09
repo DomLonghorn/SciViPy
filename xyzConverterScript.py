c89980662652f8a14112208bd31ce1c2db4622e3
FWKCarlisle <110162827+FWKCarlisle@users.noreply.github.com>
2022-08-09 14:17:01 +0100

    Lunch break additions to convertion script

--- xyzConverterScript.py
+++ xyzConverterScript.py
@@ -1,11 +1,27 @@
-Data = open("/home/user/Desktop/Data/Max data/w_220_cascade.00500.voxels.xyz","r")
-
-ConvertedFile = open("/home/user/Desktop/Testxyz","w")
-ConvertedFile.write("Cowabunga dudes \n")
-
+#Data = open("/home/user/Desktop/Data/Max data/w_220_cascade.00500.voxels.xyz","r")
+noofdatapoints = 10
 count = 0
-for x in Data:
-    if count<10:
-        ReadLine = x
-        ConvertedFile.write(ReadLine)
-        count+=1
+convertedFile = open("C:\\Users\\FWKCa\\OneDrive\\Desktop\\Max Data\\Testxyz.txt","w")
+Dimensions = []
+
+# with open("C:\\Users\\FWKCa\\OneDrive\\Desktop\\Max Data\\w_220_cascade.00500.voxels.xyz","r") as Data:
+#     datapoints = Data.readlines()[1:(noofdatapoints+2)]
+#     print(Data.readline())
+#     Dimensions.append(Data.readlines()[2])
+Data = open("C:\\Users\\FWKCa\\OneDrive\\Desktop\\Max Data\\w_220_cascade.00500.voxels.xyz","r")
+
+for x in Data range(1:(noofpoints+2)):
+    datapoints = x 
+
+
+
+
+#print(Dimensions)
+
+
+
+
+for i in range(0, noofdatapoints):
+    convertedFile.write(datapoints[i])
+    
+

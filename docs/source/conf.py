14aa3326ac2b5ef4b744ee06b198e45527cd8681
Dom Longhorn <dpl508@york.ac.uk>
2022-10-25 14:09:10 +0100

    html setup

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -14,16 +14,10 @@ release = "1.0"
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-import os
-import sys
-
-sys.path.insert(0, os.path.abspath(".."))
-
-extensions = ["sphinx.ext.autodoc", "sphinx.ext.viewcode", "sphinx.ext.napoleon"]
+extensions = ["sphinx.ext.napoleon"]
 
 templates_path = ["_templates"]
 exclude_patterns = []
-
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output

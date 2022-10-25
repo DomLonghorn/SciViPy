44dbbf9b941a7c06b5d43a77221e6d6fdbeb7758
Dom Longhorn <dpl508@york.ac.uk>
2022-10-25 11:53:23 +0100

    Moved files to scripts folder and set up Sphinx documenation

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -14,7 +14,12 @@ release = "1.0"
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = ["sphinx.ext.napoleon"]
+import os
+import sys
+
+sys.path.insert(0, os.path.abspath(".."))
+
+extensions = ["sphinx.ext.autodoc", "sphinx.ext.viewcode", "sphinx.ext.napoleon"]
 
 templates_path = ["_templates"]
 exclude_patterns = []
@@ -23,5 +28,5 @@ exclude_patterns = []
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = "alabaster"
+html_theme = "sphinx_rtd_theme"
 html_static_path = ["_static"]

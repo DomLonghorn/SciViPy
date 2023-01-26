05fcaa1e40f27907f346fa216719e5b383e70d86
chronque <csg524@york.ac.uk>
2023-01-26 22:43:32 +0000

    Added test for if the file exists; needs to be more robust

--- Testing/TEST_TIME_READER.py
+++ Testing/TEST_TIME_READER.py
@@ -1,77 +1,94 @@
-import time
+def time_reader(filename="/jorek_times.txt", noofpoints=150, start=4000, Range=607):
+    import time
+    import os
+
+    # Get directory of this file
+    dir_path = os.path.dirname(os.path.realpath(__file__))
+
+    # File to read
+    file = dir_path + filename
+
+    # File to write
+    output = dir_path + "/TestText.txt"
+
+    # Open file
+    with open(file, "r") as f:
+        # Read lines
+        FLines = f.readlines()
+        # Get length
+        FLength = len(FLines)
+
+    # Create empty lists
+    listofpoints = []
+    listOfTimesteps = []
+    listofnumbers = []
+    CleanedOutputs = []
+    FileID = []
+
+    # Range
+    count = 0
+
+    # Iterate through lines
+    for x in FLines:
+        FullString = x
+        StringToCut = FullString[0:14]
+        FinalString = FullString.replace(StringToCut, "")
+        SigFigString = FinalString[0:8]
+        StringToConvert = SigFigString.strip()
+        NumString = float(StringToConvert)
+        # print(NumString)
+
+        if count >= 553:
+            NumString = (
+                NumString * 10
+            )  # Used to handle the difference in significant figures within the dataset (probably a more general solution)
+
+        CleanedOutputs.append(NumString)
+        count += 1
+
+    # Get start value
+    startval = CleanedOutputs[1]
+
+    # Get end index
+    endindex = Range
+
+    # Get end value
+    endval = CleanedOutputs[endindex]
+
+    # Initial time step
+    initialtimestep = (endval - startval) / noofpoints
+
+    # Iterate through points
+    for i in range(noofpoints):
+        DummyTimestep = initialtimestep * i
+        listOfTimesteps.append(DummyTimestep)
+        listofnumbers.append(startval + listOfTimesteps[i])
+
+    for i in range(len(listOfTimesteps)):
+        # Find the value that's closest in the file to the given timestep and give its val
+        closestval = min(
+            enumerate(CleanedOutputs), key=lambda x: abs(x[1] - (listofnumbers[i]))
+        )
+        FileID.append(start + (10 * closestval[0]))
+
+    # Write file
+    with open(output, "w") as fp:
+        for item in FileID:
+            # write each item on a new line
+            fp.write("%s\n" % item)
+        print("Done")
+
+    fp.close()
 
 
-listofpoints = []
-listOfTimesteps = []
-listofnumbers = []
-CleanedOutputs = []
-FileID = []
-File = open("Testing/jorek_times.txt", "r")
-FLines = File.readlines()
-FLength = len(File.readlines())
-# print("FLength: ", FLength)
-# print("FLines: ", FLines)
-File.close()
+# test for time_reader:
+def test_time_reader_output_file():
+    import os
 
-# Range = 607  # The number of time steps to read from a file
-count = 0  # Incorporates significant figures (see for loop below)
-# start = 4000  # Initial value to read
-
-
-for x in FLines:
-    FullString = x
-    # print("FullString: ", FullString)
-    StringToCut = FullString[0:14]
-    FinalString = FullString.replace(
-        StringToCut, ""
-    )  # Trims the data into a floating point format to be used in numerical calculations
-    SigFigString = FinalString[0:8]
-    StringToConvert = SigFigString.strip()
-    NumString = float(StringToConvert)
-    print(NumString)
-    if count >= 553:
-        NumString = (
-            NumString * 10
-        )  # Used to handle the difference in significant figures within the dataset (probably a more general solution)
-
-    CleanedOutputs.append(NumString)
-    count += 1
-
-# print("CleanedOutputs: ", CleanedOutputs)
-startval = CleanedOutputs[1]
-
-endindex = 607
-endval = CleanedOutputs[endindex]
-noofpoints = 150
-
-initialtimestep = (
-    endval - startval
-) / noofpoints  # Used to determine the value of an equitemporal time step over a given number of pooints
-
-for i in range(
-    noofpoints
-):  # Creates a temporary timestep that then gets added to an array which is added to the initial value
-    DummyTimestep = initialtimestep * i
-    listOfTimesteps.append(DummyTimestep)
-
-    listofnumbers.append(startval + listOfTimesteps[i])
-
-
-for i in range(
-    len(listOfTimesteps)
-):  # Finds the value that's closest in the file to the given timestep and gives its val
-    closestval = min(
-        enumerate(CleanedOutputs), key=lambda x: abs(x[1] - (listofnumbers[i]))
+    time_reader(filename="/jorek_times.txt", noofpoints=150, start=4000, Range=607)
+    assert os.path.exists(
+        "/Users/csp/Library/Mobile Documents/com~apple~CloudDocs/Programming/SciViPy/Testing/TestText.txt"
     )
-    FileID.append(4000 + (10 * closestval[0]))
 
-print(FileID)
 
-with open(r"Testing/TestText.txt", "w") as fp:
-    for item in FileID:
-        # write each item on a new line
-        fp.write("%s\n" % item)
-    print("Done")
-
-fp.close()
-
+test_time_reader_output_file()
